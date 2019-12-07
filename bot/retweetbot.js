5667a3b38c0f1c74963f1af06659f85a0229feaa
Hiroaki Ogasawara <hiroga1030@gmail.com>
2019-12-07 15:47:52 +0900

    :+1: Update: some fix

--- bot/retweetbot.js
+++ bot/retweetbot.js
@@ -1,36 +1,33 @@
-// Our Twitter library
-const Twit = require('twit');
-
-// We need to include our configuration file
-const T = new Twit(require('./config.js'));
-
 const promisify = require('util').promisify
+
+const Twit = require('twit');
+const T = new Twit(require('./config.js'));
 
 const RETWEETED = 'You have already retweeted this Tweet.'
 
 const search = async (query) => {
-    const result = await promisify(T.get)('search/tweets', query).catch(error => {
+    const result = await promisify(T.get).bind(T)('search/tweets', query).catch(error => {
         console.log('There was an error with your hashtag search:', error);
     })
     return result.statuses
 }
 
 const retweet = async (tweet_id) => {
-    return await promisify(T.post)('statuses/retweet/' + tweet_id, {}).catch(error => {
+    return await promisify(T.post).bind(T)('statuses/retweet/' + tweet_id, {}).catch(error => {
         if (error.message == RETWEETED) {
-            console.log(RETWEETED)
+            console.log(RETWEETED, { tweet_id: tweet_id })
         } else {
             console.log('There was an error with Twitter:', error);
         }
     })
 }
 
-// This function finds the latest tweet with the #mediaarts hashtag, and retweets it.
-exports.retweetLatest = (event, context, callback) => {
+exports.retweetLatest = async (event, context, callback) => {
     const query = { q: process.env.query, count: 10, result_type: "recent" };
-    const statuses = search(query)
-    statuses.forEach(status => {
+    const statuses = await search(query)
+    console.log({ statuses })
+    statuses.forEach(async status => {
         const tweet_id = status.id_str
-        retweet(tweet_id)
+        await retweet(tweet_id)
     })
 }
